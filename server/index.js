1e76f5f8874a141c875b7a23f8772067500ca5c5
zbot473 <zbot473@gmail.com>
2019-04-06 14:18:44 -0400

    Server start

--- server/index.js
+++ server/index.js
@@ -1,4 +1,84 @@
-const nrf24 = require("nRF24");
-var rf24= new nrf24.RF24(24,22);
-rf24.begin();
-console.log("Radio connected:" + rf24.present()); // Prints true/false if radio is connected.
+const Influx = require('influx');
+const bodyParser = require('body-parser');
+const express = require('express');
+const exphbs = require('express-handlebars');
+const http = require('http')
+// Server app
+const app = express();
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+app.use(bodyParser.json());
+
+const influx = new Influx.InfluxDB({
+    host: 'localhost',
+    database: 'soil_measurements',
+    schema: [{
+        measurement: 'soil_moisture',
+        fields: {
+            uuid: Influx.FieldType.STRING,
+            moisture: Influx.FieldType.INTEGER
+        },
+        tags: [
+            "soil"
+        ]
+    }]
+});
+
+influx.getDatabaseNames()
+    .then(names => {
+        if (!names.includes('soil_measurements')) {
+            return influx.createDatabase('soil_measurements');
+        }
+    })
+    .then(() => {
+        http.createServer(app).listen(8010, function () {
+            console.log('Listening on port 3000...')
+        })
+    })
+    .catch(err => {
+        console.log(err)
+        console.error('Error creating Influx database!');
+    });
+
+// Root
+app.get('/', function (req, res, next) {
+    res.render('home');
+});
+
+app.get('/current', (req, res, next) => {
+    influx.query(`
+        select * from soil_moisture
+    `).then(result => {
+        ``
+        res.json(result[result.length-1])
+
+    });
+
+});
+
+app.get('/range', () => {
+    res.send({
+        x: 10
+    });
+});
+
+// Send datapoint
+app.post('/add', (req, res, next) => {
+    console.log(req.body);
+    influx.writePoints([{
+        measurement: 'soil_moisture',
+        fields: {
+            uuid: req.body.uuid,
+            moisture: req.body.moisture
+        }
+    }]).then(
+        () => {
+            res.sendStatus(200)
+        }
+    );
+
+});
+
+module.exports = app;
